51a07f8cd9cac849c36aebd01c9e7c99cf221f8c
Fernando W S Furtado <feh.wilinando@gmail.com>
2018-02-08 18:12:16 -0200

    Some Commit

--- src/main/java/br/com/caelum/mog/web/Boot.java
+++ src/main/java/br/com/caelum/mog/web/Boot.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Boot {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Boot.class, args);
     }
 
